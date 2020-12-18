b040b8ee6b9bb1e24549985acada0f4f3e34f19c
realfranser <serranoarresejavier@gmail.com>
2020-12-18 02:02:08 +0100

    Added lab3 and pdf from lab 1

--- Docu/myNotes/BuildTheModel.py
+++ Docu/myNotes/BuildTheModel.py
@@ -45,19 +45,18 @@ def read_dataset(filename, mode, batch_size=512):
  ...
  dataset = tf.data.TextLineDataset(filename).map(decode_csv)
  if mode == tf.estimator.ModeKeys.TRAIN:
- num_epochs = None # indefinitely
- dataset = dataset.shuffle(buffer_size=10*batch_size)
+     num_epochs = None # indefinitely
+     dataset = dataset.shuffle(buffer_size=10*batch_size)
  else:
- num_epochs = 1 # end-of-input after this
- dataset = dataset.repeat(num_epochs).batch(batch_size)
+     num_epochs = 1 # end-of-input after this
+     dataset = dataset.repeat(num_epochs).batch(batch_size)
 
  return dataset.make_one_shot_iterator().get_next()
 
  #TrainSpec consists of the things that used to be passed into train() method
- 
- train_spec = tf.estimator.TrainSpec(
- input_fn=read_dataset('gs://.../train*', mode=tf.contrib.learn.ModeKeys.TRAIN),
- max_steps=num_train_steps)
+train_spec = tf.estimator.TrainSpec(
+            input_fn=read_dataset('gs://.../train*', mode=tf.contrib.learn.ModeKeys.TRAIN),
+            max_steps=num_train_steps)
 ...
 tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
 
@@ -71,3 +70,11 @@ eval_spec=tf.estimator.EvalSpec(
  throttle_secs=600, # evaluate every N seconds
  exporters=exporter)
 tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
+#Wide and deep network in Estimator API
+model = tf.estimator.DNNLinearCombinedClassifier(
+ model_dir=...,
+ linear_feature_columns=wide_columns,
+ dnn_feature_columns=deep_columns,
+ dnn_hidden_units=[100, 50])
+
